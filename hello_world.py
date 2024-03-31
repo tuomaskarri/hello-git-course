832e7a33a21433cdbccaeaefdbc5fcaa9c96d7a3
Tuomas <tuomas.karri92@gmail.com>
2024-03-31 16:00:33 +0300

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+"""
+TIE-GIT git-kurssi
+hello_world
+Tekijä: Tuomas Karri
+Opiskelijanumero: 151850374
+sähköposti: tuomas.karri@tuni.fi
+"""
+
+def main():
+
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
