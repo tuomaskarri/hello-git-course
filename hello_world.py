718718a350fde34900674109d8b64b2ab35c92eb
Tuomas <tuomas.karri92@gmail.com>
2024-04-01 12:56:54 +0300

    add line

--- hello_world.py
+++ hello_world.py
@@ -6,13 +6,6 @@ Tekijä: Tuomas Karri
 Opiskelijanumero: 151850374
 sähköposti: tuomas.karri@tuni.fi
 """
-
-def main():
-
-    print("Hello World!")
-
-if __name__ == "__main__":
-    main()
-=======
 print("Hello world!")
 >>>>>>> 9bb7a505374c63502be00fe0f9bbfff438b6a484
+print("Hello from feature")
