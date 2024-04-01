84b6de88a90280eb504c6c507bed5d3f7576ecc0
Tuomas <165562534+tuomaskarri@users.noreply.github.com>
2024-04-01 13:30:35 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,1 @@
-"""
-TIE-GIT git-kurssi
-hello_world
-Tekijä: Tuomas Karri
-Opiskelijanumero: 151850374
-sähköposti: tuomas.karri@tuni.fi
-"""
 
-def main():
-
-    print("Hello world!")
-
-if __name__ == "__main__":
-    main()
